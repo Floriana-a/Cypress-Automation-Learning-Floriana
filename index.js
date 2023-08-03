0d2523fe9211119aedcc5c6c6249dee31d658905
Floriana Afrim <fafrim@deloittece.com>
2023-08-03 13:06:39 +0300

    New Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// console.log('Hello World');
+
+
+// let name = 'Wednesday';
+// //console.log(name);
+
+
+// let firstName = 'Wednesday'; //String Literal
+// let lastName = 'Addams';
+// let age = 8; //Number Literal
+// let isApproved = true; //Boolean Literal
+// //console.log(firstName);
+
+
+// let interestRate = 0.3;
+// interestRate = 1;
+// //console.log(interestRate);
+
+let person = {
+    name: 'Wednesday',
+    age: 8
+}
+console.log(person);
