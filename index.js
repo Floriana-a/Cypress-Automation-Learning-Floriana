0683e2dc8808f52b75ea51c8f98ee5fa9a273f2e
Floriana Afrim <fafrim@deloittece.com>
2023-08-09 10:18:41 +0300

    This is a new update

--- index.js
+++ index.js
@@ -1,23 +1,88 @@
-// console.log('Hello World');
+console.log('Hello World');
 
 
 // let name = 'Wednesday';
-// //console.log(name);
+// console.log(name);
 
 
-// let firstName = 'Wednesday'; //String Literal
-// let lastName = 'Addams';
-// let age = 8; //Number Literal
-// let isApproved = true; //Boolean Literal
-// //console.log(firstName);
+// // let firstName = 'Wednesday'; //String Literal
+// // let lastName = 'Addams';
+// // let age = 8; //Number Literal
+// // let isApproved = true; //Boolean Literal
+// // //console.log(firstName);
 
 
-// let interestRate = 0.3;
-// interestRate = 1;
-// //console.log(interestRate);
+// // let interestRate = 0.3;
+// // interestRate = 1;
+// // //console.log(interestRate);
 
-let person = {
-    name: 'Wednesday',
-    age: 8
-}
-console.log(person);
+// // let person = {
+// //     name: 'Wednesday',
+// //     age: 8
+// // };
+// // console.log(person);
+
+// // let x = 10;
+// // let y = 3;
+
+// // // Increment
+// // console.log(x++); // x = x +1
+// // console.log(x);
+// // //x = x + 5 -> x += 5
+// // //x = x * 3 -> x *= 3
+
+// // console.log(x + y);
+// // console.log(x - y);
+// // console.log(x * y);
+// // console.log(x / y);
+// // console.log(x % y);
+// // console.log(x ** y);
+
+// // //Decrement 
+// // console.log(-- x);
+
+// // let a = 'red';
+// // let b = 'blue';
+
+// // let c = a; //swapping variables
+// // a = b; 
+// // b = c;
+
+
+// // console.log(a);
+// // console.log(b);
+
+// // Hour
+// // If hour is between 6am and 12pm: Good Morning!
+// // If hour is between 12pm and 6pm: Good Afternoon!
+// // Otherwise: Good Evening!
+
+// // let hour = 10;
+// // if (hour >= 6 && hour <12)
+// //     console.log('Good Morning');
+
+// // else if (hour >= 12 && hour <18) 
+// //     console.log('Good Afternoon');
+
+// // else
+// // console.log('Good Evening');
+
+// let role;
+// switch (role) {
+//     case 'guest': 
+//     console.log('Guest User');
+//     break;
+
+//     case 'moderator':
+//         console.log('Moderator User');
+//         break;
+
+//         default:
+//             console.log('Unknown User');
+// }
+
+// if (role === 'guest') console.log('Guest');
+// else if (role === 'moderator') console.log('Moderator User');
+// else console.log('Unknown User');
+
+
