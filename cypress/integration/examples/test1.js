4797ac4b13854758a8f1e9a8710ae1666a98136b
Floriana Afrim <fafrim@deloittece.com>
2023-08-17 16:47:18 +0300

    Added: new update for cypress, a part of test I am working on from cypress course

--- cypress/integration/examples/test1.js
+++ cypress/integration/examples/test1.js
@@ -1,1 +1,17 @@
 // cypress - Spec
+
+// describe('My First Test', () => {
+//     it('Does not do much!', () => {
+//       expect(true).to.equal(true)
+//     })
+//   })
+
+describe('My First Test Suite', function(){
+    it('My First Test Case', function(){
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.get('.search-keyword').type('ca')
+        cy.wait(2000)
+        //selenium get hit url in browser, cypress get acts like findElement of selenium
+        cy.get('.product').should('have.length', 4)
+    })
+})
